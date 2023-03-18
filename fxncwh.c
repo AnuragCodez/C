293c1d726f15f3042ee56911558ee99f70c07762
Anurag Kumar Maurya <anuragkumarmaurya000@gmail.com>
2023-03-18 09:33:32 +0530

    print

---
+++ fxncwh.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+   
+      void printstar(int n)
+   {
+   for(int i = 0; i <n; i++)
+   {
+    
+    printf("%c",'*');
+   }
+   } 
+  
+int main()
+{
+  
+printstar(7);
+return 0;
+}
