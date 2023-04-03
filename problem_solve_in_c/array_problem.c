f713d9fe180177d4526a0ed7e4f345a0c089a5b0
Anurag Kumar Maurya <anuragkumarmaurya000@gmail.com>
2023-04-03 10:12:10 +0530

    edit

---
+++ problem_solve_in_c/array_problem.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{ 
+
+// int a[4] = {1,2,3,4};
+// int b[4]= {1,2,3,4};
+
+// for (int i = 0; i <=3; i++)
+// {
+//     if (a[i]==b[i])
+//     {
+//         printf("1");
+//     }
+//     else
+//     {
+//         printf("0");
+//     }
+// }
+
+
+
+return 0;
+}
