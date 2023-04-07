1b66d58d344c7e4755dd12e0f4ea7fd124c30a3f
Anurag Kumar Maurya <anuragkumarmaurya000@gmail.com>
2023-04-07 22:35:14 +0530

    edit

--- problem_solve_in_c/linearsearch.c
+++ problem_solve_in_c/linearsearch.c
@@ -1,7 +1,27 @@
 #include<stdio.h>
+#include<process.h>
+#include<stdlib.h>
+
 int main()
 {
+   int a[4] ={2,3,2,3};
+   int b[4] = {3,2,4,3};
+int c=1;
+   for (int i = 0; i <=3; i++)
+   {
+      if (a[i]== b[i])
+      {
+        printf("%d",c);
+        break;
+      }
+      else
+      {
+        printf("0");
+      }
+      
+   }
    
 
+   
 return 0;
 }
